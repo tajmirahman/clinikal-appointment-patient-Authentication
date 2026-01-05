33bbb542d4347faf8d7f6e7f9745f972152542ed
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2026-01-05 16:49:12 +0600

    login and register form design done

--- my-project/src/Auth/Login.jsx
+++ my-project/src/Auth/Login.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+
 
 const Login = () => {
     return (
-        <div className='w-9/12 mx-auto mt-10'>
-            <div className="card my-10 */:border w-[65%] mx-auto p-2">
-            <h2 className='text-4xl text-center'>Login Form</h2>
-                <div className="body ">
+        <div className='w-9/12 mx-auto mt-10 */:border'>
+            <div className="card my-10  w-[65%] mx-auto p-4 shadow-md">
+                <h2 className='text-4xl text-center'>Login Form</h2>
+                <div className="body p-2">
                     <fieldset className="fieldset ml-3">
                         <legend className="fieldset-legend">Email</legend>
                         <input type="email" className="input w-[70%]" placeholder="Type here email" />
@@ -13,9 +13,12 @@ const Login = () => {
                     <fieldset className="fieldset ml-3">
                         <legend className="fieldset-legend">Password</legend>
                         <input type="password" className="input w-[70%]" placeholder="Type here password" />
-                        <p className="label">Forget your password</p>
+                        <p className="label text-red-400 underline">Forget your password?</p>
                     </fieldset>
-                    <button  className='btn text-center'>Login</button>
+
+                    <div className='flex justify-center mt-3'>
+                        <button className='btn bg-[#32a3ac] text-white'>Login</button>
+                    </div>
                 </div>
             </div>
         </div>
--- my-project/src/Auth/Register.jsx
+++ my-project/src/Auth/Register.jsx
@@ -2,8 +2,32 @@ import React from 'react';
 
 const Register = () => {
     return (
-        <div>
-            This is register page
+        <div className='w-9/12 mx-auto mt-10 */:border'>
+            <div className="card my-10  w-[65%] mx-auto p-4 shadow-md">
+                <h2 className='text-4xl text-center'>Register Form</h2>
+                <div className="body p-2">
+                    <fieldset className="fieldset ml-3">
+                        <legend className="fieldset-legend">Name</legend>
+                        <input type="text" className="input w-[70%]" placeholder="Type here name" />
+                    </fieldset>
+                    <fieldset className="fieldset ml-3">
+                        <legend className="fieldset-legend">Photo Url</legend>
+                        <input type="text" className="input w-[70%]" placeholder="Type here photo url" />
+                    </fieldset>
+                    <fieldset className="fieldset ml-3">
+                        <legend className="fieldset-legend">Email</legend>
+                        <input type="email" className="input w-[70%]" placeholder="Type here email" />
+                    </fieldset>
+                    <fieldset className="fieldset ml-3">
+                        <legend className="fieldset-legend">Password</legend>
+                        <input type="password" className="input w-[70%]" placeholder="Type here password" />
+                    </fieldset>
+
+                    <div className='flex justify-center mt-3'>
+                        <button className='btn bg-[#32a3ac] text-white'>Register</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
